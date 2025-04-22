996af92c8f44dd59b9dfce50d48cf7b5c8677bfa
Dima <dimtar009@gmail.com>
2025-04-22 13:30:22 +0300

    return h-screen

--- src/app/ui/layout.tsx
+++ src/app/ui/layout.tsx
@@ -2,32 +2,18 @@ import clsx from 'clsx'
 import { Outlet } from 'react-router'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { useGetBgPath } from '../../shared/lib/theme'
-import { useEffect } from 'react'
 
 export function Layout() {
 	const queryClient = new QueryClient()
 
 	const bgPath = useGetBgPath()
-
-	useEffect(() => {
-		const setHeight = () => {
-			document.documentElement.style.setProperty(
-				'--window-height',
-				`${window.innerHeight}px`
-			)
-		}
-		setHeight() // Устанавливаем высоту при загрузке
-		window.addEventListener('resize', setHeight) // Обновляем при изменении размера окна
-		return () => window.removeEventListener('resize', setHeight) // Очищаем слушатель
-	}, [])
-
 	return (
 		<QueryClientProvider client={queryClient}>
 			<div
 				style={{ backgroundImage: `url(${bgPath})` }}
 				className={clsx(
 					'flex items-center justify-center',
-					'bg-zinc-900 h-[var(--window-height)] w-screen text-white',
+					'bg-zinc-900 h-screen w-screen text-white',
 					`bg-no-repeat bg-center`,
 					'bg-cover overflow-hidden'
 				)}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
-			height: {
-				window: 'var(--window-height)',
-			},
 			boxShadow: {
 				'soft-up': '0 -10px 15px -3px rgba(0, 0, 0, 0.3)', // Мягкая тень вверх
 				'softer-up': '0 -15px 25px -5px rgba(0, 0, 0, 0.15)', // Ещё мягче
